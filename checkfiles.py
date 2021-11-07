aa1f0c0983405d05583bb301bd0161264853e826
qwjelqweqwjehqekqewl <89981675+qwjelqweqwjehqekqewl@users.noreply.github.com>
2021-11-07 12:28:18 +0100

    Update checkfiles.py

--- checkfiles.py
+++ checkfiles.py
@@ -17,6 +17,18 @@ f11 = os.path.isfile('z3ntl3_v1.js')
 f12 = os.path.isfile('z3ntl3_v2.js')
 f13 = os.path.isfile('lky_root/proxies/rf.txt')
 f14 = os.path.isfile('lky_root/proxies/ua.txt')
+f15 = path.exists('node_modules/fs')
+f16 = path.exists('node_modules/cloudflare-bypasser')
+f17 = path.exists('node_modules/cloudscraper')
+f18 = path.exists('node_modules/cluster')
+f19 = path.exists('node_modules/crypto-random-string')
+f20 = path.exists('node_modules/flood')
+f21 = path.exists('node_modules/net')
+f22 = path.exists('node_modules/path')
+f23 = path.exists('node_modules/random-useragent')
+f24 = path.exists('node_modules/request')
+f25 = path.exists('node_modules/zombie')
+
 correct = True
 incorrect = False
 
@@ -111,6 +123,74 @@ def main():
         value = value + 1
         print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
     if f14 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f15 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+
+    if f16 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f17 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f18 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f19 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f20 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f21 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f22 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f23 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+    if f24 == correct:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
+    else:
+        value = value + 1
+        print(f"--> Lowkey Files Check {value} NOT Found! \033[31m[INCORRECT]\033[0m")
+
+    if f25 == correct:
         value = value + 1
         print(f"--> Lowkey Files Check {value} \033[32m[Correct]\033[0m")
     else:
